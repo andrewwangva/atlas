c1707f9b2d44d82277e75e90bf634ef32f4350dd
Andrew Wang <andrewwang@gmail.com>
2025-04-30 04:26:15 +0000

    af

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -1077,7 +1077,7 @@ class RayPPOTrainer(object):
                 timing_raw = {}
 
                 batch: DataProto = DataProto.from_single_dict(batch_dict)
-                print("Batch keys", batch.non_tensor_batch["uid"])
+                print("Batch keys", batch.non_tensor_batch.keys())
                 print("Batch size", batch.batch['raw_prompt_ids'].shape[0])
                 # pop those keys for generation
                 if 'multi_modal_inputs' in batch.non_tensor_batch.keys():
