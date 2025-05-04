ac3b5a1eb23fe8164221689ada9d70d1269b8eab
Andrew Wang <andrewwang@gmail.com>
2025-05-04 17:52:49 +0000

    remove j

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -1045,11 +1045,10 @@ class RayPPOTrainer(object):
         """
         selected_indices = []
         for i in range(len(data)):
-            for j in range(len(data[i].batch['responses'])):
-                if data[i].batch['responses'][j].size(1) < max_len:
-                    selected_indices.append(i)
-                else:
-                    print("Response length: ", data[i].batch['responses'][j].size(1))
+            if data[i].batch['responses'].size(1) < max_len:
+                selected_indices.append(i)
+            else:
+                print("Response length: ", data[i].batch['responses'].size(1))
         
         filtered_data = [data[i] for i in selected_indices]
         filtered_data = item_collate_fn(filtered_data)
